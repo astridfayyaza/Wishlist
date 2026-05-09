060ca49acb3aab322d04694108e1637f307c4026
astridfayyaza <fayyazaastrid@gmail.com>
2026-05-10 00:50:45 +0700

    Implement navigation graph in MainActivity

--- app/src/main/java/com/astrid0049/wishlist/MainActivity.kt
+++ app/src/main/java/com/astrid0049/wishlist/MainActivity.kt
@@ -3,28 +3,19 @@ package com.astrid0049.wishlist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.astrid0049.wishlist.nav.SetupNavGraph
 import com.astrid0049.wishlist.ui.theme.WishlistTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             WishlistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SetupNavGraph()
             }
         }
     }
